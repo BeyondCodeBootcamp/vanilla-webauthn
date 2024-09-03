145626629bd928691622a353f77ddecff25ca769
AJ ONeal <aj@therootcompany.com>
2024-09-02 22:29:39 -0600

    fix: use URL-safe Base64, correct log messages

--- app.js
+++ app.js
@@ -33,18 +33,27 @@ PassKey.auth = {};
 PassKey.textEncoder = new TextEncoder();
 // let textDecoder = new TextDecoder();
 
-///**
-// * @param {Uint8Array|ArrayBuffer} buffer
-// */
-//PassKey._bufferToBase64 = function (buffer) {
-//  let bytes = new Uint8Array(buffer);
-//  //@ts-ignore
-//  let binstr = String.fromCharCode.apply(null, bytes);
-//  return btoa(binstr);
-//}
+/**
+ * @param {Uint8Array|ArrayBuffer?} buffer
+ */
+PassKey._bufferToBase64 = function (buffer) {
+  if (!buffer?.byteLength) {
+    return null;
+  }
+
+  let bytes = new Uint8Array(buffer);
+  //@ts-ignore
+  let binstr = String.fromCharCode.apply(null, bytes);
+  let rfcBase64 = btoa(binstr);
+
+  let urlBase64 = rfcBase64.replace(/=+$/g, "");
+  urlBase64 = urlBase64.replace(/[/]/g, "_");
+  urlBase64 = urlBase64.replace(/[+]/g, "-");
+  return urlBase64;
+};
 
 /**
- * @param {Uint8Array|ArrayBuffer?} [buffer]
+ * @param {Uint8Array|ArrayBuffer?} buffer
  */
 PassKey._bufferToHex = function (buffer) {
   if (!buffer) {
@@ -141,11 +150,11 @@ PassKey.reg.responseToJSON = function (cred) {
   let authenticatorData = attResp.getAuthenticatorData();
   let asn1Pubkey = attResp.getPublicKey();
   let coseKeyType = attResp.getPublicKeyAlgorithm();
-  let keyType = "";
+  let keyTypeName = "";
   if (coseKeyType === -7) {
-    keyType = "ES256";
+    keyTypeName = "ES256";
   } else if (-257) {
-    keyType = "RS256";
+    keyTypeName = "RS256";
   } else {
     throw new Error(
       `COSE algorithm #${coseKeyType} is not known to be widely supported`,
@@ -156,15 +165,21 @@ PassKey.reg.responseToJSON = function (cred) {
   let jsonCred = {
     authenticatorAttachment: cred.authenticatorAttachment,
     id: cred.id,
-    rawId: PassKey._bufferToHex(cred.rawId),
+    rawId: PassKey._bufferToBase64(cred.rawId), // same as cred.id
+    rawIdHex: PassKey._bufferToHex(cred.rawId), // same as cred.id
     response: {
-      attestationObject: PassKey._bufferToHex(attResp.attestationObject),
+      attestationObject: PassKey._bufferToBase64(attResp.attestationObject),
+      attestationObjectHex: PassKey._bufferToHex(attResp.attestationObject),
       // https://developer.mozilla.org/en-US/docs/Web/API/Web_Authentication_API/Authenticator_data
       // a subset of attestationObject
-      authenticatorData: PassKey._bufferToHex(authenticatorData),
-      clientDataJSON: PassKey._bufferToHex(attResp.clientDataJSON),
-      publicKey: PassKey._bufferToHex(asn1Pubkey),
-      publicKeyAlgorithm: keyType,
+      authenticatorData: PassKey._bufferToBase64(authenticatorData),
+      authenticatorDataHex: PassKey._bufferToHex(authenticatorData),
+      clientDataJSON: PassKey._bufferToBase64(attResp.clientDataJSON),
+      clientDataJSONHex: PassKey._bufferToHex(attResp.clientDataJSON),
+      publicKey: PassKey._bufferToBase64(asn1Pubkey),
+      publicKeyHex: PassKey._bufferToHex(asn1Pubkey),
+      publicKeyAlgorithm: coseKeyType,
+      publicKeyAlgorithmName: keyTypeName,
       transports: attResp.getTransports(),
     },
     type: cred.type,
@@ -237,6 +252,8 @@ PassKey.auth.defaultOpts = {
 
 /**
  * Converts a WebAuthn Public Key Credential response to plain JSON with base64 encoding for byte array fields.
+ * Note: 'userHandle' is a client-side secret, or at least it  allows up to 64-bytes,
+ * some of which may be used for a local encryption key.
  * https://developer.mozilla.org/en-US/docs/Web/API/AuthenticatorAssertionResponse
  * @param {void|PublicKeyCredential?} cred
  */
@@ -247,17 +264,25 @@ PassKey.auth.responseToJSON = function (cred) {
 
   /** @type {AuthenticatorAssertionResponse} */ //@ts-ignore
   let assResp = cred.response;
+  let userHandleHex = PassKey._bufferToHex(assResp.userHandle);
 
   // Convert the credential response to plain JSON
   let jsonCred = {
     authenticatorAttachment: cred.authenticatorAttachment,
     id: cred.id,
-    rawId: PassKey._bufferToHex(cred.rawId),
+    rawId: PassKey._bufferToBase64(cred.rawId), // same as cred.id
+    rawIdHex: PassKey._bufferToHex(cred.rawId), // same as cred.id
     response: {
-      authenticatorData: PassKey._bufferToHex(assResp.authenticatorData),
-      clientDataJSON: PassKey._bufferToHex(assResp.clientDataJSON),
-      signature: PassKey._bufferToHex(assResp.signature),
-      userHandle: PassKey._bufferToHex(assResp.userHandle),
+      authenticatorData: PassKey._bufferToBase64(assResp.authenticatorData),
+      authenticatorDataHex: PassKey._bufferToHex(assResp.authenticatorData),
+      clientDataJSON: PassKey._bufferToBase64(assResp.clientDataJSON),
+      clientDataJSONHex: PassKey._bufferToHex(assResp.clientDataJSON),
+      signature: PassKey._bufferToBase64(assResp.signature),
+      signatureHex: PassKey._bufferToHex(assResp.signature),
+      getClientSecretUserHandleHex: function () {
+        // this is a client-side secret and should NOT be disclosed to the server
+        return userHandleHex;
+      },
     },
     type: cred.type,
   };
@@ -288,6 +313,9 @@ PassKey.reg.set = async function (
     pubkeyRegOpts.publicKey.challenge = PassKey._challenge;
   }
 
+  console.log(`rp.id:`, pubkeyRegOpts.publicKey.rp.id);
+  let challengeHex = PassKey._bufferToHex(PassKey._challenge);
+  console.log(`challenge: ${challengeHex}`);
   let pubkeyRegResp = await navigator.credentials
     .create(pubkeyRegOpts)
     .catch(function (err) {
@@ -328,6 +356,9 @@ PassKey.auth.getOrWaitFor = async function (authRequestOpts, abortMsg) {
     authRequestOpts.publicKey.challenge = PassKey._challenge;
   }
 
+  console.log(`rpId:`, authRequestOpts.publicKey.rpId);
+  let challengeHex = PassKey._bufferToHex(PassKey._challenge);
+  console.log(`challenge: ${challengeHex}`);
   let pubkeyAuthResp = await navigator.credentials
     .get(authRequestOpts)
     .catch(function (err) {
@@ -406,7 +437,7 @@ PassUI.reg.createOrReplaceKey = async function (event) {
     $mediation.onchange();
   }
 
-  console.log("PassKey.reg.set() pubkeyRegOpts", pubkeyRegOpts);
+  console.log("PassUI.reg.createOrReplaceKey() pubkeyRegOpts", pubkeyRegOpts);
   let regResult = await PassKey.reg
     .set(pubkeyRegOpts, abortMsg)
     .then(function (regResult) {
@@ -424,7 +455,10 @@ PassUI.reg.createOrReplaceKey = async function (event) {
     let abortMsg = "change from canceled registration to additional login";
     let authRequestOpts = globalThis.structuredClone(PassKey.auth.defaultOpts);
     authRequestOpts.mediation = PassUI.auth.mediation;
-    console.log("getPublicKey() authRequestOpts", authRequestOpts);
+    console.log(
+      "PassUI.reg.createOrReplaceKey() authRequestOpts [conditional]",
+      authRequestOpts,
+    );
     void (await PassKey.auth
       .getOrWaitFor(authRequestOpts, abortMsg)
       .then(PassUI.auth.handleResult));
@@ -454,7 +488,7 @@ PassUI.auth.requestKey = async function (event) {
   // (or to not show the current user in a login-is prompt)
   // excludeCredentials: [ { type: 'public-key', id: 'base64id' }]
 
-  console.log("getPublicKey() authRequestOpts", authRequestOpts);
+  console.log("PassUI.auth.requestKey() authRequestOpts", authRequestOpts);
   void (await PassKey.auth
     .getOrWaitFor(authRequestOpts, abortMsg)
     .then(PassUI.auth.handleResult)
@@ -474,7 +508,7 @@ PassUI.setAttachment = async function (event) {
     let authRequestOpts = globalThis.structuredClone(PassKey.auth.defaultOpts);
     authRequestOpts.mediation = PassUI.auth.mediation;
 
-    console.log("getPublicKey() authRequestOpts", authRequestOpts);
+    console.log("PassUI.setAttachment(): authRequestOpts", authRequestOpts);
     void (await PassKey.auth
       .getOrWaitFor(authRequestOpts, abortMsg)
       .then(PassUI.auth.handleResult)
@@ -504,7 +538,7 @@ PassUI.auth.setMediation = async function (event) {
     let authRequestOpts = globalThis.structuredClone(PassKey.auth.defaultOpts);
     authRequestOpts.mediation = PassUI.auth.mediation;
 
-    console.log("getPublicKey() authRequestOpts", authRequestOpts);
+    console.log("PassUI.setMediation(): authRequestOpts", authRequestOpts);
     void (await PassKey.auth
       .getOrWaitFor(authRequestOpts, abortMsg)
       .then(PassUI.auth.handleResult)
@@ -538,7 +572,6 @@ PassUI.reg.handleResult = async function (regResp) {
 
   console.log(`PassUI.reg.handleResult (opaque):`);
   console.log(regResp);
-
   let regResult = PassKey.reg.responseToJSON(regResp);
   console.log(`PassUI.reg.handleResult (JSON):`);
   console.log(regResult);
@@ -555,7 +588,6 @@ PassUI.auth.handleResult = async function (authResp) {
 
   console.log(`PassUI.auth.handleResult (opaque):`);
   console.log(authResp);
-
   let authResult = PassKey.auth.responseToJSON(authResp);
   console.log(`PassUI.auth.handleResult (JSON):`);
   console.log(authResult);
